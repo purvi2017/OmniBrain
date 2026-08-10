2e217ed8a39a7f37901e755d44ef8b7f3f9bdfa5
Purvi Patel <purvi477@gmail.com>
2026-08-10 15:51:48 +0530

    Added backend initial setup

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="OmniBrain API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "🚀 OmniBrain API Running Successfully!"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Healthy"
+    }
