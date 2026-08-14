72aafc0974e6a274c6ae5aa528a035f14ed1dc36
Purvi Patel <purvi477@gmail.com>
2026-08-15 00:06:49 +0530

    Day 4 - Added frontend landing page and PDF upload module

---
+++ frontend/js/app.js
@@ -0,0 +1,37 @@
+async function uploadPDF() {
+
+    const fileInput = document.getElementById("pdfFile");
+    const status = document.getElementById("status");
+
+    if (fileInput.files.length === 0) {
+        status.innerText = "Please select a PDF file";
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    try {
+
+        const response = await fetch(
+            "http://127.0.0.1:8000/upload",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        console.log("Status:", response.status);
+
+        const text = await response.text();
+
+        console.log("Response:", text);
+
+        status.innerText = "Upload Successful ✅";
+
+    } catch (error) {
+
+        console.error(error);
+        status.innerText = "Upload Failed ❌";
+    }
+}
